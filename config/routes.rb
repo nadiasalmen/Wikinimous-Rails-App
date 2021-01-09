6eb908d586e54a1a716bac9da35a1d542ef9ff26
Nadia Salmen <nadiasalmen@gmail.com>
2021-01-09 15:38:09 -0300

    Create article routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # read all
+  get 'articles', to: 'articles#index', as: :articles
+
+  # create one
+  get 'articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
+
+  # read one - show must go after the "new" routes
+  get 'artciles/:id', to: 'articles#show', as: :article
+
+  # edit one
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+
+  # delete one
+  delete 'articles/:id', to: 'articles#destroy'
 end
