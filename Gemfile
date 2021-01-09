3600e48eed81597dcc3c80bfb85d3e8f93190923
Nadia Salmen <nadiasalmen@gmail.com>
2021-01-09 17:12:24 -0300

    User story: as a user i can view the content of an article

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'faker'
 # Add simple form
 gem 'simple_form'
+# Add fontawesome
+gem 'font-awesome-sass'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -14,7 +16,6 @@ class ArticlesController < ApplicationController
   end
 
   def show
-
   end
 
   def edit
@@ -33,5 +34,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
